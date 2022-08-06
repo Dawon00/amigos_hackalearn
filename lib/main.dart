e2067fdc5a12b26bb8db059c611cbef1e5433d16
Hyeonjae-K <href.hjk@gmail.com>
2022-08-07 00:58:21 +0900

    FEAT - 파이어베이스 연동 구현

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,21 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  if (kIsWeb) {
+    await Firebase.initializeApp(
+      options: const FirebaseOptions(
+        apiKey: "AIzaSyBzH4R5PlQk5gB3qRTViK0K5MzwqArhY5w",
+        appId: "1:612032563687:web:c848e0f1ab08287d6b5542",
+        messagingSenderId: "612032563687",
+        projectId: "amigos-hackalearn",
+      ),
+    );
+  } else {
+    await Firebase.initializeApp();
+  }
   runApp(const MyApp());
 }
 
