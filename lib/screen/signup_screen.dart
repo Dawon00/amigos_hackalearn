bdf43f6d1f9bc35dbe9225aae90f6046c4ee35bd
Hyeonjae-K <href.hjk@gmail.com>
2022-08-07 22:45:24 +0900

    FEAT - 회원가입 기능 구현

---
+++ lib/screen/signup_screen.dart
@@ -0,0 +1,237 @@
+import 'dart:typed_data';
+
+import 'package:amigos/model/user.dart' as model;
+import 'package:amigos/widget/input_field.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+
+class SignupScreen extends StatefulWidget {
+  const SignupScreen({Key? key}) : super(key: key);
+
+  @override
+  State<SignupScreen> createState() => _SignupScreenState();
+}
+
+class _SignupScreenState extends State<SignupScreen> {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  final TextEditingController _usernameController = TextEditingController();
+  Uint8List? _image;
+  bool _isLoading = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      body: SafeArea(
+        child: Container(
+          padding: const EdgeInsets.symmetric(horizontal: 32),
+          width: double.infinity,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Flexible(
+                flex: 2,
+                child: Container(),
+              ),
+              // logo
+              Image.asset(
+                'assets/light_logo.png',
+                height: 120,
+              ),
+              const SizedBox(
+                height: 16,
+              ),
+              // image field
+              Stack(
+                children: [
+                  _image != null
+                      ? CircleAvatar(
+                          radius: 64,
+                          backgroundImage: MemoryImage(_image!),
+                        )
+                      : const CircleAvatar(
+                          radius: 64,
+                          backgroundImage: AssetImage('assets/dark_logo.png'),
+                        ),
+                  Positioned(
+                    bottom: -10,
+                    left: 80,
+                    child: IconButton(
+                      onPressed: () async {
+                        Uint8List img = await pickImage(ImageSource.gallery);
+
+                        setState(() {
+                          _image = img;
+                        });
+                      },
+                      icon: const Icon(Icons.add_a_photo),
+                    ),
+                  ),
+                ],
+              ),
+              // username field
+              InputField(
+                textEditingController: _usernameController,
+                hintText: 'Username',
+              ),
+              // email field
+              InputField(
+                textEditingController: _emailController,
+                hintText: 'Email',
+                inputType: TextInputType.emailAddress,
+              ),
+              // password field
+              InputField(
+                textEditingController: _passwordController,
+                hintText: 'Password',
+                isPassword: true,
+              ),
+              const SizedBox(
+                height: 16,
+              ),
+              // signup button
+              InkWell(
+                onTap: () async {
+                  setState(() {
+                    _isLoading = true;
+                  });
+
+                  String res = _image != null
+                      ? await signUpUser(
+                          email: _emailController.text,
+                          password: _passwordController.text,
+                          username: _usernameController.text,
+                          file: _image!,
+                        )
+                      : 'Please select your profile image';
+
+                  if (res == "success") {
+                    Navigator.of(context).pushReplacement(
+                      MaterialPageRoute(
+                        builder: (context) => Text(res),
+                      ),
+                    );
+                  } else {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(
+                        content: Text(res),
+                      ),
+                    );
+                  }
+
+                  setState(() {
+                    _isLoading = false;
+                  });
+                },
+                child: Container(
+                  width: double.infinity,
+                  alignment: Alignment.center,
+                  padding: const EdgeInsets.symmetric(vertical: 12),
+                  decoration: const ShapeDecoration(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(4),
+                      ),
+                    ),
+                    color: Color.fromRGBO(0, 149, 246, 1),
+                  ),
+                  child: _isLoading
+                      ? const Center(
+                          child: CircularProgressIndicator(
+                            color: Colors.white,
+                          ),
+                        )
+                      : const Text(
+                          'Log in',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                ),
+              ),
+              Flexible(
+                flex: 2,
+                child: Container(),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _emailController.dispose();
+    _passwordController.dispose();
+    _usernameController.dispose();
+  }
+
+  Future pickImage(ImageSource source) async {
+    final ImagePicker imagePicker = ImagePicker();
+
+    XFile? file = await imagePicker.pickImage(source: source);
+
+    if (file != null) {
+      return await file.readAsBytes();
+    }
+  }
+
+  Future<String> signUpUser({
+    required String email,
+    required String password,
+    required String username,
+    required Uint8List file,
+  }) async {
+    late String res;
+
+    try {
+      final FirebaseAuth _auth = FirebaseAuth.instance;
+      final FirebaseStorage _storage = FirebaseStorage.instance;
+      final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
+      if (email.isEmpty ||
+          password.isEmpty ||
+          username.isEmpty ||
+          file.isEmpty) {
+        res = "Please enter all the fields";
+      } else {
+        UserCredential cred = await _auth.createUserWithEmailAndPassword(
+          email: email,
+          password: password,
+        );
+
+        Reference ref =
+            _storage.ref().child('profileImages').child(_auth.currentUser!.uid);
+        UploadTask uploadTask = ref.putData(file);
+        TaskSnapshot snapshot = await uploadTask;
+        String photoUrl = await snapshot.ref.getDownloadURL();
+
+        model.User user = model.User(
+          email: email,
+          uid: cred.user!.uid,
+          username: username,
+          photoUrl: photoUrl,
+          implements: [],
+          saved: 0,
+        );
+
+        await _firestore.collection('users').doc(cred.user!.uid).set(
+              user.toJson(),
+            );
+
+        res = "success";
+      }
+    } catch (e) {
+      res = e.toString();
+    }
+
+    return res;
+  }
+}
