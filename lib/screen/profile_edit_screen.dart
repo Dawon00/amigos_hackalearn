85886e236d91b67d8b751610c1b3c992a03a5625
Hyeonjae-K <href.hjk@gmail.com>
2022-08-10 00:42:16 +0900

    FIX - 마이 페이지 이동시 uid 인자 추가

---
+++ lib/screen/profile_edit_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class ProfileEditScreen extends StatefulWidget {
+  const ProfileEditScreen({Key? key}) : super(key: key);
+
+  @override
+  State<ProfileEditScreen> createState() => _ProfileEditScreenState();
+}
+
+class _ProfileEditScreenState extends State<ProfileEditScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
