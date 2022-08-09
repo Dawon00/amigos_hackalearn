209d56b0709af2022822555cc31921ffaba82a38
Hyeonjae-K <href.hjk@gmail.com>
2022-08-10 00:40:29 +0900

    FEAT - 마이 페이지 사용자 정보 및 페이지 이동 기능 구현

--- lib/screen/my_page_screen.dart
+++ lib/screen/my_page_screen.dart
@@ -1,18 +1,147 @@
+import 'package:amigos_hackalearn/model/user.dart' as model;
+import 'package:amigos_hackalearn/screen/login_screen.dart';
+import 'package:amigos_hackalearn/screen/profile_edit_screen.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class MyPageScreen extends StatefulWidget {
-  const MyPageScreen({Key? key}) : super(key: key);
+  final String uid;
+  const MyPageScreen({Key? key, required this.uid}) : super(key: key);
 
   @override
   State<MyPageScreen> createState() => _MyPageScreenState();
 }
 
 class _MyPageScreenState extends State<MyPageScreen> {
+  late final model.User user;
+  bool isLoading = false;
+
+  void setUser() async {
+    setState(() {
+      isLoading = true;
+    });
+
+    try {
+      DocumentSnapshot userSnap = await FirebaseFirestore.instance
+          .collection('users')
+          .doc(widget.uid)
+          .get();
+      user = model.User.fromSnap(userSnap);
+    } catch (e) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text(e.toString()),
+        ),
+      );
+    }
+
+    setState(() {
+      isLoading = false;
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    setUser();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text('마이 페이지')),
-      body: Text('content'),
-    );
+    return isLoading
+        ? const Center(
+            child: CircularProgressIndicator(),
+          )
+        : Scaffold(
+            appBar: AppBar(
+              title: Text(user.username),
+              centerTitle: false,
+            ),
+            body: ListView(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(16),
+                  child: Column(
+                    children: [
+                      Row(
+                        children: [
+                          CircleAvatar(
+                            backgroundImage: NetworkImage(user.photoUrl),
+                            radius: 40,
+                          ),
+                          Expanded(
+                            child: Column(
+                              children: [
+                                Container(
+                                  alignment: Alignment.centerLeft,
+                                  padding: const EdgeInsets.all(8),
+                                  child: const Text(
+                                    '지금까지',
+                                    textAlign: TextAlign.left,
+                                    style: TextStyle(
+                                      fontSize: 14,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                ),
+                                Container(
+                                  alignment: Alignment.centerRight,
+                                  child: Text(
+                                    '아낀 금액은 ${user.saved.toString()}원',
+                                    textAlign: TextAlign.end,
+                                  ),
+                                ),
+                                Container(
+                                  alignment: Alignment.centerRight,
+                                  child: Text(
+                                    '총 ${user.implements.length.toString()}일 절약 실천중',
+                                    textAlign: TextAlign.end,
+                                  ),
+                                ),
+                                Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceEvenly,
+                                  children: [
+                                    TextButton(
+                                      onPressed: () {
+                                        if (!mounted) return;
+                                        Navigator.of(context).pushReplacement(
+                                          MaterialPageRoute(
+                                            builder: (context) =>
+                                                const ProfileEditScreen(),
+                                          ),
+                                        );
+                                      },
+                                      child: const Text('프로필 편집'),
+                                    ),
+                                    TextButton(
+                                      onPressed: () async {
+                                        FirebaseAuth _auth =
+                                            FirebaseAuth.instance;
+                                        await _auth.signOut();
+                                        if (!mounted) return;
+                                        Navigator.of(context).pushReplacement(
+                                          MaterialPageRoute(
+                                            builder: (context) =>
+                                                const LoginScreen(),
+                                          ),
+                                        );
+                                      },
+                                      child: const Text('로그아웃'),
+                                    ),
+                                  ],
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          );
   }
 }
