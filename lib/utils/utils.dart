185a798bfbeb37c16b709c0e146c82f3a1437eb9
kimwest00 <kimwest00@hufs.ac.kr>
2022-08-13 12:55:52 +0900

    REFACTOR(issue#4) - upload_image 함수 utils.dart로 이동

---
+++ lib/utils/utils.dart
@@ -0,0 +1,11 @@
+import 'dart:typed_data';
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+
+Future pickImage(ImageSource source) async {
+  final ImagePicker imagePicker = ImagePicker();
+  XFile? file = await imagePicker.pickImage(source: source);
+  if (file != null) {
+    return await file.readAsBytes();
+  }
+}
