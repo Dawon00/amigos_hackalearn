82f6f2d0ccf8c18e621aff2f1cad4eb9bd22d56f
Dawon00 <da6921@naver.com>
2022-08-17 02:58:50 +0900

    FEAT - mypage profile 부분 shadow 디자인 추가

--- lib/screen/my_page_screen.dart
+++ lib/screen/my_page_screen.dart
@@ -234,6 +234,15 @@ class _MyPageScreenState extends State<MyPageScreen> {
                               width: double.infinity,
                               padding: EdgeInsets.all(10),
                               decoration: BoxDecoration(
+                                boxShadow: [
+                                  BoxShadow(
+                                    color: ButtonColor.withOpacity(0.2),
+                                    spreadRadius: 5,
+                                    blurRadius: 7,
+                                    offset: Offset(
+                                        0, 3), // changes position of shadow
+                                  ),
+                                ],
                                 borderRadius: BorderRadius.circular(10),
                                 color: primaryColor.withOpacity(0.5),
                               ),
